a8e4c9b46a1c205081b455b8560d3e17c7796313
unknown <your@email.example>
2025-09-29 09:52:20 +0300

    form.blade.php

---
+++ resources/views/form.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <title>Forma</title>
+</head>
+<body>
+    <h1>Ievades forma</h1>
+    <form action="/submit" method="POST">
+        @csrf
+        <label for="name">Vārds:</label>
+        <input type="text" id="name" name="name"><br><br>
+
+        <label for="email">E-pasts:</label>
+        <input type="email" id="email" name="email"><br><br>
+
+        <button type="submit">Sūtīt</button>
+    </form>
+</body>
+</html>
