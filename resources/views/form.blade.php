cf4e5dba66d4ea0b60abc20d42b8b7e84517d857
unknown <your@email.example>
2025-09-29 11:06:04 +0300

    sss

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -6,15 +6,15 @@
 </head>
 <body>
     <h1>Ievades forma</h1>
-    <form action="/submit" method="POST">
-        @csrf
-        <label for="name">Vārds:</label>
-        <input type="text" id="name" name="name"><br><br>
+    <form action="/submit.php" method="POST">
+    @csrf
+    <label for="name">Vārds:</label>
+    <input type="text" id="name" name="name"><br><br>
 
-        <label for="email">E-pasts:</label>
-        <input type="email" id="email" name="email"><br><br>
+    <label for="email">E-pasts:</label>
+    <input type="email" id="email" name="email"><br><br>
 
-        <button type="submit">Sūtīt</button>
-    </form>
+    <button type="submit">Sūtīt</button>
+</form>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/form', function () {
     return view('form');
+});
+
+Route::get('/static-form', function () {
+    return view('static_form');
+});
+
+
+Route::post('/submit-static-form', function (Request $request) {
+    // Log submitted data (for debugging)
+    \Log::debug($request->all());
+
+    // Optionally, return a simple confirmation response
+    return "Form submitted successfully! Received: " . json_encode($request->all());
 });
